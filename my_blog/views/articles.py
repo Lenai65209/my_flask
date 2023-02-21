bae31fd40b034ca597696c4de63d5bbdeb089fd1
Lenai65209 <97385676+Lenai65209@users.noreply.github.com>
2023-02-22 00:50:20 +0300

    Lesson 2 Елена Иконникова. Группа 2141. Поток Flask от 16.02.2023.  (#2)
    * start flask commit
    
    * Lesson 2
    
    * from Lesson 2

---
+++ my_blog/views/articles.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, render_template
+from werkzeug.exceptions import NotFound
+
+from .users import USERS
+
+articles_app = Blueprint("articles_app", __name__)
+ARTICLES = {
+    1: {'title': "Flask", "text": "text Flask", "author": 1},
+    2: {'title': "Django", "text": "text Django", "author": 2},
+    3: {'title': "JSON:APIo", "text": "text JSON:API", "author": 3},
+}
+
+
+@articles_app.route("/", endpoint="list")
+def articles_list():
+    return render_template("articles/list.html", articles=ARTICLES)
+
+
+@articles_app.route("/<int:article_id>/", endpoint="details")
+def article_details(article_id: int):
+    try:
+        article_name = ARTICLES[article_id]['title']
+        article_text = ARTICLES[article_id]['text']
+        article_author_id = ARTICLES[article_id]['author']
+        article_author = USERS[article_author_id]
+    except KeyError:
+        raise NotFound(f"Article #{article_id} doesn't exist!")
+    return render_template('articles/details.html', article_id=article_id,
+                           article_name=article_name, article_text=article_text,
+                           article_author=article_author,
+                           article_author_id=article_author_id)
