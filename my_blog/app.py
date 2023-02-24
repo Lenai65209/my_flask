7b3c4cf8cb0bd74d75c90cbb602f320ea5742a2c
Lenai65209 <97385676+Lenai65209@users.noreply.github.com>
2023-02-24 08:36:16 +0300

    Update app.py

--- my_blog/app.py
+++ my_blog/app.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template
-from my_blog.views.users import users_app
-from my_blog.views.articles import articles_app
+import os
 
+from flask import Flask, render_template
+
+from my_blog.models.database import db
+from my_blog.views.articles import articles_app
+from my_blog.views.auth import login_manager, auth_app
+from my_blog.views.users import users_app
 
 app = Flask(__name__)
 app.register_blueprint(users_app)
 app.register_blueprint(articles_app, url_prefix="/articles")
 
-# @app.route("/")
-# def index():
-#     return "Hello web!"
+file_path = os.path.abspath(os.getcwd()) + "\database.db"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + file_path
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+app.config["SECRET_KEY"] = "123456"
+app.register_blueprint(auth_app, url_prefix="/auth")
+login_manager.init_app(app)
+
 
 @app.route("/")
 def index():
