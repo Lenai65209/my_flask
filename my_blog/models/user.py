dedabd45fdbabe4869b726d14e657f07ec0cb0e9
Lenai65209 <lenai65209@rambler.ru>
2023-02-28 07:42:24 +0300

    Lesson 4

--- my_blog/models/user.py
+++ my_blog/models/user.py
@@ -1,13 +1,15 @@
-from sqlalchemy import Column, Integer, String, Boolean
 from flask_login import UserMixin
+from sqlalchemy import Column, Integer, String, Boolean
 
-from my_blog.models.database import db
+from .database import db
 
 
 class User(db.Model, UserMixin):
     id = Column(Integer, primary_key=True)
     username = Column(String(80), unique=True, nullable=False)
     is_staff = Column(Boolean, nullable=False, default=False)
+    email = Column(String(255), nullable=False, default="", server_default="")
+    admin = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return f"<User #{self.id} {self.username!r}>"
